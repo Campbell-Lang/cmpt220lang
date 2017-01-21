89dd08981bd78f5b7c16b5d4755c67f18d4d5b75
Campbell Lang <campbell.lang1@marist.edu>
2017-01-21 15:17:28 -0500

    Project 0
    Java file + screenshot of Kattis

---
+++ prj/Zero/driver_proj0.java
@@ -0,0 +1,9 @@
+package cmpt220;
+
+public class driver_proj0 {
+	public static void main(String[] args) 
+	{
+		// display message welcome to Java on the console
+		System.out.println("Hello World!");
+	}
+}
