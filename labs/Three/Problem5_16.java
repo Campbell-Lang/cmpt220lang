a192d410165f9a9188d57d134b82c9937d6e9a53
Campbell Lang <campbell.lang1@marist.edu>
2017-02-04 12:26:19 -0500

    Problem 5.16

---
+++ labs/Three/Problem5_16.java
@@ -0,0 +1,24 @@
+package cmpt220;
+
+import java.util.Scanner;
+
+public class Problem5_16 {
+	public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first integer number: ");
+        int num = input.nextInt();
+        input.close();
+
+        for (int i = 2; i < num; ) {
+
+            if (num % i == 0) {
+                System.out.print(i +" ");
+                num /= i;
+            } else {
+                i++;
+            }
+        }
+    }
+
+}
