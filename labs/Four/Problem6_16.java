0b392a81ad46bacf05251ec5e96c1a94af4f9fef
Campbell Lang <campbell.lang1@marist.edu>
2017-02-13 16:30:46 -0500

    Problem 6.16

---
+++ labs/Four/Problem6_16.java
@@ -0,0 +1,36 @@
+package cmpt220;
+
+import java.util.Scanner;
+
+public class Problem6_16 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		//Prompt the user to enter year 
+		System.out.print("Enter full year (e.g. 2016): ");
+		int year = input.nextInt();
+		
+		System.out.print("The number of days this year is: " + getTotalNumberOfDays(year));
+
+	}
+	
+	public static int getTotalNumberOfDays(int year) {
+		int total = 0;
+		
+		//get the total days from 1800 to 1/1/year {
+			if(isLeapYear(year))
+				total = 366;
+			else 
+				total = 365;
+			return total;
+		}
+		
+		
+	
+	
+	public static boolean isLeapYear(int year) {
+		return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+	}
+
+}
