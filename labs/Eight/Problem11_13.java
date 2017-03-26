a0f36e5b194bcbe2e3fbc916a817d0a39d2aa617
Campbell Lang <campbell.lang1@marist.edu>
2017-03-26 14:50:54 -0400

    Problem 11.13

---
+++ labs/Eight/Problem11_13.java
@@ -0,0 +1,31 @@
+package cmpt220;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Problem11_13 {
+	public static void main(String[] args) {
+
+        System.out.print("Enter 10 integers: ");
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) list.add(input.nextInt());
+
+        System.out.println("Removing duplicates...");
+        removeDuplicate(list);
+        System.out.println("Displaying list...");
+        System.out.println(list);
+    }
+
+    public static void removeDuplicate(ArrayList<Integer> list) {
+
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+
+            if (!temp.contains(list.get(i))) {
+                temp.add(list.get(i));
+            }
+        }
+        list.clear();
+        list.addAll(temp);
+
+    }
+}
