00a9e0b36626fc24a520d166494dd991bd939087
Campbell Lang <campbell.lang1@marist.edu>
2017-04-17 22:03:58 -0400

    Problem 13.11

---
+++ labs/Ten/Octagon.java
@@ -0,0 +1,39 @@
+package cmpt220;
+
+public class Octagon extends GeometricObject{
+	 double side;
+
+
+	    public Octagon() {
+	        this(5);
+	    }
+
+	    public Octagon(double side) {
+	        this.side = side;
+	    }
+
+	    @Override
+	    public double getArea() {
+	        return (2.0 + 4.0 / Math.sqrt(2) * side * side);
+	    }
+
+	    @Override
+	    public double getPerimeter() {
+	        return side * 8;
+	    }
+
+	    public Object clone() {
+	        Object o = null;
+	        try {
+	            o = super.clone();
+	        } catch (CloneNotSupportedException ex) {
+	            ex.printStackTrace();
+	        }
+	        return o;
+	    }
+
+	    @Override
+	    public boolean equals(Object o) {
+	        return o instanceof Octagon && getArea() == ((Octagon)o).getArea();
+	    }
+}
