2c578aa892e8919035e66e94c880f510e3f8e52f
Campbell Lang <campbell.lang1@marist.edu>
2017-04-17 21:54:16 -0400

    Problem 13.5

---
+++ labs/Ten/MyPoint.java
@@ -0,0 +1,23 @@
+package cmpt220;
+
+public class MyPoint {
+
+	public static double x = 0;
+	public static double y = 0;
+
+	public MyPoint(double x, double y) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public double distance(double x, double y) {
+		this.x = x;
+		this.y = y;
+		return x;
+	}
+
+	public Object getCenterPoint(MyPoint lowest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
